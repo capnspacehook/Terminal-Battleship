b2ba685573eabd29af50f94588e475a35e5fc61f
JeremyShahan <shahanj@oregonstate.edu>
2020-03-03 15:02:21 -0800

    Added automatic testing to barrel detonation unit

--- Test.cpp
+++ Test.cpp
@@ -2,31 +2,99 @@
 #include "GameGrid.h"
 #include "PlayerGrid.h"
 #include "ComputerGrid.h"
+#include <ctime>
 using namespace std;
 
+int test_barrel_detonate(int, int, int);
+void test_barrel();
+
+
 int main() {
-  int diffChoice;
-  bool turnResult;
-  cout << "Welcome to the unit testing area!\n" << endl
-       << "Please choose your difficulty:" << endl
-       << "(1) Easy" << endl
-       << "(2) Medium" << endl
-       << "(3) Hard\n: ";
-    cin >> diffChoice;
-    while (cin.fail() || diffChoice < 1 || diffChoice > 3)
-    {
-      cin.clear();
-      cin.ignore(numeric_limits<streamsize>::max(), '\n');
-      cout << "That is not a valid difficulty setting." << endl
-           << "Please enter a valid difficulty setting: ";
-      cin >> diffChoice;
+
+  test_barrel();
+
+ 
+}
+
+
+
+void test_barrel() {
+
+  int s_total = 0;
+  int m_total = 0;
+  int l_total = 0;
+  srand(time(0));
+
+  for (int i=0; i<20; i++) {
+    int rand_x = (rand() % 8);
+    int rand_y = (rand() % 8);
+    s_total += test_barrel_detonate(rand_x, rand_y, 1);
+  }
+
+
+  for (int i=0; i<20; i++) {
+    int rand_x = (rand() % 10);
+    int rand_y = (rand() % 10);
+    m_total += test_barrel_detonate(rand_x, rand_y, 2);
+  }
+
+
+  for (int i=0; i<20; i++) {
+    int rand_x = (rand() % 12);
+    int rand_y = (rand() % 12);
+    l_total += test_barrel_detonate(rand_x, rand_y, 3);
+  }
+
+
+  if (s_total == 0) {
+    cout << endl << "All small boards passed the barrel detonation test" << endl;
+  }
+  else {
+    cout << endl << s_total << " small board(s) failed the barrel detonation test" << endl;
+  }
+
+
+  if (m_total == 0) {
+    cout << endl << "All medium boards passed the barrel detonation test" << endl;
+  }
+  else {
+    cout << endl << m_total << " medium board(s) failed the barrel detonation test" << endl;
+  }
+
+
+  if (l_total == 0) {
+    cout << endl << "All large boards passed the barrel detonation test" << endl;
+  }
+  else {
+    cout << endl << l_total << " large board(s) failed the barrel detonation test" << endl;
+  }
+
+}
+
+
+
+int test_barrel_detonate(int x, int y, int diff) {
+  bool temp = true;
+  GameGrid grid(diff, true);
+  grid.barrelDetonation(x, y, temp);
+  for (int i=-2; i<3; i++) {
+    for (int j=-2; j<3; j++) {
+      if ((i == 0) && (j == 0)) {
+	if (grid.getGridCell(x,y) != '#') {
+	cout << endl << "this function failed with this board" << endl;
+	grid.printGrid();
+	return 1;
+	}
+      }
+      else if ((x+i < grid.getDifficultySize()) && (x+i >= 0) && (y+j < grid.getDifficultySize()) && (y+j >= 0)) {
+        if (grid.getGridCell(x+i,y+j) != 'X') {
+          cout << endl << "this function failed with this board" << endl;
+          grid.printGrid();
+          return 1;
+        }
+      }
     }
+  }
 
-
-    ComputerGrid computerGrid(diffChoice, true);
-    computerGrid.printGrid();
-    computerGrid.setGridCell(0,0,'*');
-    computerGrid.fire(0,0,turnResult);
-
-
+  return 0;
 }
