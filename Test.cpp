c424cbdcb6f10dd951e72b79692c234e091d38a5
JeremyShahan <shahanj@oregonstate.edu>
2020-03-03 09:45:39 -0800

    minor changes to test.cpp

--- Test.cpp
+++ Test.cpp
@@ -6,9 +6,24 @@ using namespace std;
 
 int main()
 {
+  cout << "Welcome to the unit testing area!\n" << endl
+       << "Please choose your difficulty:" << endl
+       << "(1) Easy" << endl
+       << "(2) Medium" << endl
+       << "(3) Hard\n: ";
+    cin >> diffChoice;
+    while (cin.fail() || diffChoice < 1 || diffChoice > 3)
+    {
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << "That is not a valid difficulty setting." << endl
+           << "Please enter a valid difficulty setting: ";
+      cin >> diffChoice;
+    }
+
+    PlayerGrid playerGrid(diffChoice, true);
 
 
-  cout << "Welcome to the unit testing area!\n" << endl;
 
 }
 
